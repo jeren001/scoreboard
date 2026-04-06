2c3a4d8f3f63d70f92efccf7803e428397319060
Jeren <yusupova.jerennn@gmail.com>
2026-04-06 15:07:07 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let countEl1 = document.getElementById("count-el1");
+let countEl2 = document.getElementById("count-el2");
+let count1 = 0;
+let count2 = 0;
+
+function add1() {
+    count1 += 1;
+    countEl1.textContent = count1;
+}
+
+function add2() {
+    count1 += 2;
+    countEl1.textContent = count1;
+}
+
+function add3() {
+    count1 += 3;
+    countEl1.textContent = count1;
+}
+
+function add4() {
+    count2 += 1;
+    countEl2.textContent = count2;
+}
+
+function add5() {
+    count2 += 2;
+    countEl2.textContent = count2;
+}
+
+function add6() {
+    count2 += 3;
+    countEl2.textContent = count2;
+}
